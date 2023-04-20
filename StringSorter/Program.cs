8d9056a3d8bc663df067346a6609b679ca2acf42
Thomas U <ToomanyPacketsglobal@gmail.com>
2023-04-20 22:45:11 +0200

    Add User Import

--- StringSorter/Program.cs
+++ StringSorter/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿﻿using System;
+
+class StringSorter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wählen Sie eine Sortiermethode:");
+        Console.WriteLine("1 - Quick Sort");
+        Console.WriteLine("2 - Bubble Sort");
+        Console.WriteLine("3 - Selection Sort");
+        int method = int.Parse(Console.ReadLine());
+
+        // Zufälligen String generieren
+        string inputString = GenerateRandomString();
+
+        // String sortieren
+        switch (method)
+        {
+            case 1:
+                inputString = QuickSort(inputString);
+                break;
+            case 2:
+                inputString = BubbleSort(inputString);
+                break;
+            case 3:
+                inputString = SelectionSort(inputString);
+                break;
+            default:
+                Console.WriteLine("Ungültige Auswahl");
+                return;
+        }
+
+        Console.WriteLine("Sortierte Zeichenfolge: " + inputString);
+    }
